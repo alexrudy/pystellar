750a53b22606bd74e959550eebddbf9e22631982
Alex Rudy <alex.rudy@gmail.com>
2012-11-01 01:44:48 -0700

    Cleaned up HW output

--- homeworks/ps3.py
+++ homeworks/ps3.py
@@ -8,6 +8,8 @@
 #  Copyright 2012 Alexander Rudy. All rights reserved.
 # 
 
+import time
+
 import numpy as np
 
 from astropysics.constants import Rs, Ms, Lsun
@@ -20,11 +22,12 @@ from pystellar.threading import ObjectsManager,ObjectPassthrough,EngineManager,O
 print "Stellar Structure Problem Set #2"
 X = 0.700
 Y = 0.280
-epsilon = 0.001
+epsilon = 100
 Opacity = ObjectThread(OpacityTable,ikwargs=dict(fkey='GN93hz',X=X,Y=Y),locking=True)
 Opacity.start()
 
-print "Problem #4:"
+print ""
+print "Problem #4&5:"
 
 print "Settings for the Sun"
 print "M = %10.6g g" % Ms
@@ -32,19 +35,20 @@ print "X = %10.3f" % X
 print "Y = %10.3f" % Y
 print u"ε = %10.3f ergs/g" % epsilon
 print u"μ = %10.3f" % mmw(X=X,Y=Y)
+print ""
 
 print "Outer Boundary Conditions"
 print "    Inital Guess:"
 print "    R = %10.6e cm" % Rs
 print "    L = %10.6e erg/s" % Lsun
-(r,l,P,T) = outer_boundary(R=Rs,L=Lsun,M=Ms,mu=mmw(X=X,Y=Y),optable=Opacity)
+r,l,P,T = outer_boundary(R=Rs,L=Lsun,M=Ms,mu=mmw(X=X,Y=Y),optable=Opacity,Piter=True)
 print "At m=M:"
-print "    R = %10.6e cm" % r
-print "    L = %10.6e erg/s" % l
+print "    r = %10.6e cm" % r
+print "    l = %10.6e erg/s" % l
 print "    P = %10.6e Dyne/cm^2" % P
 print "    T = %10.6e K" % T
 print "Derivatives"
-(dr,dl,dP,dT) = derivatives(np.array([Ms]),np.atleast_2d([r,l,P,T]),mu=mmw(X=X,Y=Y),optable=Opacity,epsilon=epsilon)
+dr,dl,dP,dT = derivatives(np.array([Ms]),np.atleast_2d([r,l,P,T]),mu=mmw(X=X,Y=Y),optable=Opacity,epsilon=epsilon)
 print "    dr = %10.6e" % dr
 print "    dl = %10.6e" % dl
 print "    dP = %10.6e" % dP
@@ -53,8 +57,10 @@ print ""
 
 
 # Guess from polytropes
-Pc = 1.2e17
-Tc = 1.4e7
+# Pc = 1.2e17
+# Tc = 1.4e7
+Pc = 2.477e17
+Tc = 1.571e7
 m = 1e-30*Ms
 
 print "Inner Boundary Conditions"
@@ -64,9 +70,9 @@ print "    T = %10.6e K" % Tc
 print "    Initial Step:"
 print "    m = %10.6e g" % m
 (r,l,P,T) = inner_boundary(Pc=Pc,Tc=Tc,M=Ms,mu=mmw(X=X,Y=Y),m=m,epsilon=epsilon,optable=Opacity)
-print "At M=m:"
-print "    R = %10.6e cm" % r
-print "    L = %10.6e erg/s" % l
+print "At m=m:"
+print "    r = %10.6e cm" % r
+print "    l = %10.6e erg/s" % l
 print "    P = %10.6e Dyne/cm^2" % P
 print "    T = %10.6e K" % T
 print "Derivatives"
