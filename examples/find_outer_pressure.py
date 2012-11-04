41af0f7404adc1a20a8c46c371f10ab5f9115df3
Alex Rudy <alex.rudy@gmail.com>
2012-11-04 15:09:23 -0800

    Find outer pressure now produces more diagnostics
    - Difference between iterable and non iterable modes.
    - Tests of Derivatives in Array Mode
    - Tests of Inner Pressure and Temperature Equations

--- examples/find_outer_pressure.py
+++ examples/find_outer_pressure.py
@@ -35,6 +35,7 @@ print u"ε = %10.3f ergs/g" % epsilon
 print u"μ = %10.3f" % mmw(X=X,Y=Y)
 
 print "Outer Boundary Conditions"
+print "Testing Outer Presure Conversion Mehtods"
 print "    Inital Guess:"
 print "    R = %10.6e cm" % Rs
 print "    L = %10.6e erg/s" % Lsun
@@ -46,8 +47,49 @@ r,l,P,T = outer_boundary(R=Rs,L=Lsun,M=Ms,mu=mmw(X=X,Y=Y),optable=Opacity,Piter=
 print "Absolute time taken: %g" % (time.clock() - start)
 print "At m=M:"
 print "    -- Algebraic -------------- | -- Iterable -------------- | -- Delta --------| "
-print "    R = %10.6e cm,        | R = %10.6e cm        | dR = %11.4e |" % (r,ri,(r-ri)/r)
-print "    L = %10.6e erg/s,     | L = %10.6e erg/s     | dL = %11.4e |" % (l,li,(l-li)/l)
-print "    P = %10.6e Dyne/cm^2, | P = %10.6e Dyne/cm^2 | dP = %11.4e |" % (P,Pi,(P-Pi)/P)
-print "    T = %10.6e K,         | T = %10.6e K         | dK = %11.4e |" % (T,Ti,(T-Ti)/T)
+print u"    R = %10.6e cm,        | R = %10.6e cm        | ΔR = %11.4e |" % (r,ri,(r-ri)/r)
+print u"    L = %10.6e erg/s,     | L = %10.6e erg/s     | ΔL = %11.4e |" % (l,li,(l-li)/l)
+print u"    P = %10.6e Dyne/cm^2, | P = %10.6e Dyne/cm^2 | ΔP = %11.4e |" % (P,Pi,(P-Pi)/P)
+print u"    T = %10.6e K,         | T = %10.6e K         | ΔT = %11.4e |" % (T,Ti,(T-Ti)/T)
+
+
+Pc = 2.477e17
+Tc = 1.571e7
+m = 1e-30*Ms
+print ""
+print "Inner Boundary Conditions"
+print "Testing Delta Movement of Temperature & Pressure from Boundary"
+print "    Initial Guess: (polytrope)"
+print "    P = %10.6e Dyne/cm^2" % Pc
+print "    T = %10.6e K" % Tc
+print "    Initial Step:"
+print "    m = %10.6e g" % m
+(rs,ls,Ps,Ts) = inner_boundary(Pc=Pc,Tc=Tc,M=Ms,mu=mmw(X=X,Y=Y),m=m,epsilon=epsilon,optable=Opacity)
+(rc,lc,Pc,Tc) = (0,0,Pc,Tc)
+print "At m=m:"
+print "    r = %10.6e cm" % rs
+print "    l = %10.6e erg/s" % ls
+print "    P = %10.6e Dyne/cm^2" % Ps
+print "    T = %10.6e K" % Ts
+print "    -- Initial ---------------- | -- 1st Step -------------- | -- Delta --------| "
+print u"    R = %10.6e cm,        | R = %10.6e cm        | ΔR = %11.4e |" % (rs,rc,abs(rs-rc)/rs)
+print u"    L = %10.6e erg/s,     | L = %10.6e erg/s     | ΔL = %11.4e |" % (ls,lc,abs(ls-lc)/ls)
+print u"    P = %10.6e Dyne/cm^2, | P = %10.6e Dyne/cm^2 | ΔP = %11.4e |" % (Ps,Pc,abs(Ps-Pc)/Ps)
+print u"    T = %10.6e K,         | T = %10.6e K         | ΔT = %11.4e |" % (Ts,Tc,abs(Ts-Tc)/Ts)
+
+
+
+
+print "Derivatives"
+print "Testing in Array Mode"
+dT, dP, dl, dr = derivatives(np.array([m]),np.atleast_2d([[ri,li,Pi,Ti],[rs,ls,Ps,Ts]]),mu=mmw(X=X,Y=Y),optable=Opacity,epsilon=epsilon)
+# print derivs
+print "    dr = [%14.6e,%14.6e]" % tuple(dr)
+print "    dl = [%14.6e,%14.6e]" % tuple(dl)
+print "    dP = [%14.6e,%14.6e]" % tuple(dP)
+print "    dT = [%14.6e,%14.6e]" % tuple(dT)
+# print ""
+
+
+
 Opacity.stop()
