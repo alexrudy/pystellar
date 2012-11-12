Fixing conv vs. rad gradient math

pystellar/stellar.py
@@ -88,7 +88,7 @@ def dTdm(m,r,l,P,T,rho,optable):
     rgrad = radiative_gradient(T=T,P=P,l=l,m=m,rho=rho,optable=optable)
     grad = np.zeros(rgrad.shape)
     grad[:] = rgrad # Schwartzchild criterion
-    grad[rgrad > gradT_ad] = gradT_ad
+    grad[rgrad < gradT_ad] = gradT_ad
     return -(G*m*T)/(4 * np.pi * np.power(r,4) * P) * grad
     
 def radiative_gradient(T,P,l,m,rho,optable):