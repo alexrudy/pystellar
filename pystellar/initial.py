1a228f4775ee2c76d5b306fc27e60979349ac35b
Alex Rudy <alex.rudy@gmail.com>
2012-11-11 16:20:19 -0800

    Attempt to use log-initial modes, but doesn't work

--- pystellar/initial.py
+++ pystellar/initial.py
@@ -49,7 +49,7 @@ Outer Boundary
 from __future__ import division
 import numpy as np
 
-from .density import density
+from .density import density, ldensity
 from .stellar import radiative_gradient
 from .energy import epp, eCNO
 
@@ -66,10 +66,7 @@ def inner_boundary(Pc,Tc,M,mu,m,optable,X,XCNO,cfg,convective=True):
     :param mu: Mean Molecular Weight, :math:`\mu`
     :param m: Initial mass step from center of star.
     :returns: Tuple of `(r,l,P,T)`, the four equation of state values at point m.
-    
-    .. todo::
-        I'm not sure if this is right... the local luminosity should probably depend on nuclear energy generation rates.
-    
+        
     """
     rhoc = density(P=Pc,T=Tc,mu=mu)
     epsilon = (epp(T=Tc,rho=rhoc,X=X,c=cfg) + eCNO(T=Tc,rho=rhoc,X=X,XCNO=XCNO,c=cfg))
@@ -78,6 +75,27 @@ def inner_boundary(Pc,Tc,M,mu,m,optable,X,XCNO,cfg,convective=True):
     P = inner_pressure(Pc=Pc,rho=rhoc,m=m)
     T = inner_temperature(Tc=Tc,Pc=Pc,rho=rhoc,m=m,epsilon=epsilon,optable=optable,convective=convective)
     return (r,l,P,T)
+    
+def log_inner_boundary(Pc,Tc,lM,mu,lm,optable,X,XCNO,cfg,convective=True):
+    r"""Load our inner boundary conditions for our star in Log Space.
+    
+    :param Pc: Central Pressure, :math:`P_c`
+    :param Tc: Central Temperature, :math:`T_c`
+    :param lM: Total Star Mass, :math:`\log(M_*)`
+    :param mu: Mean Molecular Weight, :math:`\mu`
+    :param m: Initial mass step from center of star, :math:`\log(dm)`
+    :returns: Tuple of `(\log(r),\log(l),P,T)`, the four equation of state values at point m.
+    """
+    lrhoc = ldensity(logP=np.log10(Pc),logT=np.log10(Tc),mu=mu)
+    rhoc = np.power(10,lrhoc)
+    m = np.power(10,lm)
+    epsilon = (epp(T=Tc,rho=rhoc,X=X,c=cfg) + eCNO(T=Tc,rho=rhoc,X=X,XCNO=XCNO,c=cfg))
+    r = inner_radius(lrho=lrhoc,lm=lm)
+    l = m * epsilon
+    P = inner_pressure(Pc=Pc,rho=rhoc,m=m)
+    T = inner_temperature(Tc=Tc,Pc=Pc,rho=rhoc,m=m,epsilon=epsilon,optable=optable,convective=convective)
+    return (r,l,P,T)
+    
     
 def outer_boundary(R,L,M,mu,optable,Piter=False):
     r"""Load the outer boundary conditions for our star.
@@ -108,6 +126,36 @@ def outer_boundary(R,L,M,mu,optable,Piter=False):
         log.warning("No pressure convergance around P=[%g,%g]: Cost Remaining=%g" % (Pguess1,Pguess2,cost))
     return (r,l,P,T)
     
+def log_outer_boundary(lR,lL,lM,mu,optable,Piter=False):
+    r"""Load the outer boundary conditions for our star.
+    
+    :param lR: Stellar Radius, :math:`\log(R_*)`
+    :param lL: Total Luminosity, :math:`\log(L_*)`
+    :param lM: Total Mass, :math:`\log(M_*)`
+    :param mu: Mean Molecular Weight :math:`\mu`
+    :param function opfunc: A function which takes :math:`\log(T)` and :math:`\log(\rho)` and returns the rosseland mean opacity, :math:`\bar{\kappa}`
+    :returns: Tuple of `(\log(r),\log(l),P,T)`, the four equation of state values at point m.
+    
+    """
+    from scipy.optimize import fminbound
+    
+    lr = lR
+    ll = L
+    T = outer_temperature(R=np.power(10,lR),L=np.power(10,lL))
+    if Piter:
+        if float(Piter) == 1.0:
+            raise ValueError, "Can't iterate with multiplicative changes of =1.0"
+        Pguess1 = find_pressure_guess(Pguess=1e1,T=T,mu=mu,optable=optable,incr=Piter)
+        Pguess2 = find_pressure_guess(Pguess=1e10,T=T,mu=mu,optable=optable,incr=1/Piter)
+    else:
+        Pbound, Tbound = get_pressure_guess(T=T,mu=mu,optable=optable)
+        Pguess2, Pguess1 = Pbound
+    P, cost, ierr, numiter = fminbound(func=outer_pressure_cost,args=(np.power(10,lR),T,np.power(10,lM),mu,optable),x1=Pguess1,x2=Pguess2,full_output=True)
+    if ierr:
+        log.warning("No pressure convergance around P=[%g,%g]: Cost Remaining=%g" % (Pguess1,Pguess2,cost))
+    return (lr,ll,P,T)
+    
+    
     
 def inner_radius(rho,m):
     r"""Inner radius to use, as a function of central density and initial step mass.
@@ -120,6 +168,19 @@ def inner_radius(rho,m):
     :param m: Initial mass step from center of star.
     """
     return np.power(3/(4*np.pi*rho)*m,1/3)
+    
+def log_inner_radius(lrho,lm):
+    r"""Inner radius to use, as a function of central density and initial step mass.
+    
+    .. math::
+       
+       \log(r) = \frac{1}{3}\log(\frac{3}{4\pi}) + \frac{1}{3}(m - \rho_c)
+    
+    :param rho: Central density, :math:`\rho_c`
+    :param m: Initial mass step from center of star.
+    """
+    print lm - lrho
+    return (1/3) * (np.log10(3/(4*np.pi)) + lm - lrho)
     
 def inner_pressure(Pc,rho,m):
     r"""Inner pressure boundary of the system, given a specified density and mass step, and a central pressure.
@@ -134,6 +195,7 @@ def inner_pressure(Pc,rho,m):
     from .constants import G
     Pdelt = ((3*G)/(8*np.pi) * np.power((4*np.pi)/3 * rho, 4/3) * np.power(m,2/3))
     return Pc - Pdelt
+    
     
 def inner_temperature(Tc,Pc,rho,m,epsilon,optable,convective=True):
     r"""Inner temperature for the core.
