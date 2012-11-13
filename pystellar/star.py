2e5842f8d224cfc698a2cdac6e84028738e5874a
Alex Rudy <alex.rudy@gmail.com>
2012-11-12 16:25:46 -0800

    Better integrator control

--- pystellar/star.py
+++ pystellar/star.py
@@ -15,28 +15,32 @@ from AstroObject.config import DottedConfiguration
 
 import logging
 from multiprocessing import Process
+from StringIO import StringIO
 
-from pystellar.opacity import OpacityTable
-from pystellar.threading import ObjectThread, ObjectManager
-
+from .opacity import OpacityTable
+from .threading import ObjectThread, ObjectManager
 from .initial import inner_boundary, log_inner_boundary, log_outer_boundary, outer_boundary
-from .stellar import derivatives, log_derivatives, radiative_gradient, dldm
+from .stellar import derivatives, log_derivatives, radiative_gradient, grad, dldm
 from .density import mmw, density
 from .integrator import integrate
 
 class Star(object):
     """A simple configured star object."""
-    def __init__(self, filename="Star.yml", optable_args=None, dashboard_args=None):
+    def __init__(self, config=None, optable_args=None, dashboard_args=None):
         super(Star, self).__init__()
         self.log = logging.getLogger(__name__)
-        self._scipy = True
-        self._logmode = True
+        self.data_log = logging.getLogger("data")
+        self.telem_log = logging.getLogger("telemetry")
+        self.telem_log.propagate = False
         self._call_count = 0
-        self._filename = filename
-        
-        self._config = DottedConfiguration()
-        self._config.load(self._filename)
+                
+        self._config = DottedConfiguration(config)
         self._config.dn = DottedConfiguration
+        
+        self._integrator_name = self.config.get("System.Integrator.Method","scipy")
+        self._integrator = getattr(self,self._integrator_name)
+        self._logmode = self.config.get("System.Integrator.LogScale",True)
+        np.set_printoptions(**self.config["System.Numpy.PrintOptions"])
         
         if optable_args is None:
             self.log.debug("Starting Opacity Table from Scratch")
@@ -62,23 +66,9 @@ class Star(object):
         self.dm_Guess = float(self._config["Star.Initial.dm"]) * Ms
         self.fp = float(self._config["Star.Integration.fp"]) * self.M
         
-        self._update_frequency = self._config["System.Dashboard.Update"]
+        self._update_frequency = self.config["System.Dashboard.Update"]
+        self._save_frequency = self.config["System.Outputs.Update"]
         
-    def use_logmode(self):
-        """Use logarithmic mass variable"""
-        self._logmode = True
-        
-    def disable_logmode(self):
-        """Don't use logarithmic mass variable."""
-        self._logmode = False
-        
-    def use_scipy(self):
-        """Toggle Scipy On"""
-        self._scipy = True
-        
-    def use_pystellar(self):
-        """Toggle Scipy Integrator Off"""
-        self._scipy = False
         
     def set_fitting_point(self,point):
         """Set a new fitting point for this routine"""
@@ -91,63 +81,50 @@ class Star(object):
         self.R_Guess = R
         self.L_Guess = L
         
-    def kill(self):
-        """Kill any processes we own!"""
-        if isinstance(self._opacity,Process):
-            self._opacity.kill()
-            
-    def stop(self):
-        """Stop any processes we own!"""
-        if isinstance(self._opacity,Process):
-            self._opacity.stop()
-        
-        
-    def log_integral(self,y,lx,i):
-        """Logarithmic Integral Function"""
-        lx = np.asarray(lx)
-        y = np.asarray(y)
-        self._call_count += 1            
-        dy = log_derivatives(xs=lx,ys=y,mu=self.mu,optable=self.opacity,X=self.X,XCNO=self.Z,cfg=self.config["Data.Energy"])[:,0]
-        if (y < 0).any():
-            self.log.warning("%s: Negative Values Encountered: \n%r, \n%r, \n%r" % (i,lx,y,dy))
-        else:
-            self.log.debug("%s: Derivs: %r, %r, %r" % (i,lx,y,dy))
-        if self._update_frequency != 0 and self._call_count % self._update_frequency == 0:
-            x = np.power(10,lx)
-            self.dashboard.insert_data(x,y,i)
-            rho = density(P=y[2],T=y[3],mu=self.mu)
-            self.dashboard.add_density(x,rho,i)
-            self.dashboard.add_epsilon(x,dy[1],i)
-            self.log.info("%d Calls to Integrator \n(x= %g) \n(y= %r) \n(dy= %r)" % (self._call_count,x,y,dy))
-            self.dashboard.update()
-        return dy
-        
     def integral(self,y,x,i):
         """docstring for integral"""
         x = np.asarray(x)
         y = np.asarray(y)
         self._call_count += 1            
-        dy = derivatives(xs=x,ys=y,mu=self.mu,optable=self.opacity,X=self.X,XCNO=self.Z,cfg=self.config["Data.Energy"])[:,0]
-        rho = density(P=y[2],T=y[3],mu=self.mu)
+        
+        dy = self.fprime(xs=x,ys=y,mu=self.mu,optable=self.opacity,X=self.X,XCNO=self.Z,cfg=self.config["Data.Energy"])[:,0]
+        
+        if self._logmode:
+            x = np.power(10,x)
+        update = self._update_frequency != 0 and self._call_count % self._update_frequency == 0
+        telem  = self._save_frequency != 0 and self._call_count % self._save_frequency == 0
+        if (y < 0).any() or update or telem:
+            rho = density(P=y[2],T=y[3],mu=self.mu)
+            eps = dy[1]
+            self.opacity.kappa(T=y[3],rho=rho)
+            rgrad = radiative_gradient(T=y[3],P=y[2],l=y[1],m=x,rho=rho,optable=self.opacity)
+            agrad = grad(rgrad)
+            self.opacity.kappa(T=y[3],rho=rho)
+            kappa = self.opacity.retrieve()
         if (y < 0).any():
-            self.log.warning("%s: Negative Values Encountered: \n%r, \n%r, \n%r" % (i,x,y,dy))
-        else:
-            self.log.debug("%s: \nx=%r, \ny=%r, \ndy=%r, \nrho=%g" % (i,x,y,dy,rho))
-        if self._update_frequency != 0 and self._call_count % self._update_frequency == 0:
+            self.log.warning(u"%s y<0 at: \nx=%r, \ny=%r, \ndy=%r, \nρ=%g \nε=%g \n∇=%g \nκ=%g" % (i,x,y,dy,rho,eps,agrad,kappa[0]))
+        if update:
             self.dashboard.insert_data(x,y,i)
             self.dashboard.add_density(x,rho,i)
             self.dashboard.add_epsilon(x,dy[1],i)
-            self.log.info("%d Calls to Integrator \n(x= %g) \n(y= %r) \n(dy= %r)" % (self._call_count,x,y,dy))
+            self.log.info(u"%s %d calls at: \nx=%r, \ny=%r, \ndy=%r, \nρ=%g \nε=%g \n∇=%g \nκ=%g" % (i,self._call_count,x,y,dy,rho,eps,agrad,kappa[0]))
             self.dashboard.update()
+        if telem:
+            self.telem_log.info(u"%r %r %r %g %g %g %g" % (x,y,dy,rho,eps,agrad,kappa[0]))
         return dy
+        
+    @property
+    def integrator(self):
+        """The actual integrator function!"""
+        return self._integrator
         
     @property
     def fprime(self):
         """The correct fprime function for integration"""
         if self._logmode:
-            return self.log_integral
+            return log_derivatives
         else:
-            return self.integral
+            return derivatives
         
         
     def show_surface_start(self):
@@ -167,20 +144,19 @@ class Star(object):
         self.log.debug("Getting Inner Boundaries")
         center_ic = inner_boundary(
             Pc=self.Pc_Guess,Tc=self.Tc_Guess,M=self.M,mu=self.mu,m=self.dm_Guess,
-            optable=self.opacity,X=self.X,XCNO=self.Z,cfg=self.config["Data.Energy"],convective=self.config["Star.Initial.Convective"])
+            optable=self.opacity,X=self.X,XCNO=self.XCNO,cfg=self.config["Data.Energy"],convective=self.config["Star.Initial.Convective"])
         if self._logmode:
-            integrator = "LogInner"
+            integrator = "LogCenter"
             ms = np.linspace(np.log10(self.dm_Guess),np.log10(self.fp),self._config["System.Outputs.Size"])
         else:
-            integrator = "Inner"
+            integrator = "Center"
             ms = np.logspace(np.log10(self.dm_Guess),np.log10(self.fp),self._config["System.Outputs.Size"])
             
         self.log.debug("Inner Conditions (%s): x=%g, y=%r" % (integrator,self.dm_Guess,np.array(center_ic)))
         self.log.debug("Starting %s Integration" % integrator)
-        if self._scipy:
-            return self.scipy(ms,center_ic,integrator)
-        else:
-            return self.pystellar(ms,center_ic,integrator)
+        
+        return self.integrator(ms,center_ic,integrator)
+        
         
     def surface(self):
         """Run an integration from the surface to the inner edge"""
@@ -189,23 +165,20 @@ class Star(object):
         (r,l,P,T) = outer_ic
         
         if self._logmode:
-            integrator = "LogOuter"
+            integrator = "LogSurface"
             ms = np.linspace(np.log10(self.fp),np.log10(self.M),self._config["System.Outputs.Size"])[::-1]
         else:
-            integrator = "Outer"
+            integrator = "Surface"
             ms = np.logspace(np.log10(self.fp),np.log10(self.M),self._config["System.Outputs.Size"])[::-1]
         
         self.log.debug("Surface Conditions (%s): x=%g, y=%r" % (integrator,self.M,np.array(outer_ic)))
         self.log.debug("Starting %s Integration" % integrator)
-        if self._scipy:
-            return self.scipy(ms,outer_ic,integrator)
-        else:
-            return self.pystellar(ms,outer_ic,integrator)
+        return self.integrator(ms,outer_ic,integrator)
         
     
     def pystellar(self,xs,ics,integrator):
         """Run an integration from the central point to the outer edge."""
-        xs,ys,xc,yc = integrate(self.fprime,xs,ics,args=(integrator,),**self.config["System.Integrator.PyStellar"][integrator]["Arguments"])
+        xs,ys,xc,yc = integrate(self.integral,xs,ics,args=(integrator,),**self.config["System.Integrator.PyStellar"][integrator]["Arguments"])
         self.log.debug("Finished %s Integration" % integrator)
         return ys, xs, None
         
@@ -213,13 +186,25 @@ class Star(object):
         """Run an integration from the central point to the outer edge."""
         self.log.debug("Calling %s Scipy Integration" % integrator)
         import scipy.integrate
-        ys, data = scipy.integrate.odeint(self.fprime,ics,xs,args=(integrator,),full_output=True,**self.config["System.Integrator.Scipy"][integrator]["Arguments"])
-        self.log.debug("Finished %s Integration" % integrator)
+        ys, data = scipy.integrate.odeint(self.integral,ics,xs,args=(integrator,),
+            full_output=True,**self.config["System.Integrator.Scipy"][integrator]["Arguments"])
+        self.log.debug("Finished %s Integration: %d derivative calls." % (integrator,self._call_count))
         if self._logmode:
             xs = np.power(10,xs)
-        self.dashboard.replace_data(xs,ys,integrator)
         rho = density(P=ys[:,2],T=ys[:,3],mu=self.mu)
-        eps = dldm(T=ys[:,3],rho=rho,X=self.X,XCNO=self.Z,cfg=self.config["Data.Energy"])
+        eps = dldm(T=ys[:,3],rho=rho,X=self.X,XCNO=self.XCNO,cfg=self.config["Data.Energy"])
+        self.opacity.kappa(T=ys[:,3],rho=rho)
+        rgrad = radiative_gradient(T=ys[:,3],P=ys[:,2],l=ys[:,1],m=xs,rho=rho,optable=self.opacity)
+        agrad = grad(rgrad)
+        self.opacity.kappa(T=ys[:,3],rho=rho)
+        kappa = self.opacity.retrieve()
+        all_data = np.vstack(map(np.atleast_2d,(xs,ys.T,rho,eps,rgrad,agrad,kappa))).T
+        stream = StringIO()
+        np.savetxt(stream,all_data)
+        self.data_log.info(stream.getvalue())
+        stream.close()
+        self.dashboard.replace_data(xs,ys,integrator)
+
         # self.opacity.kappa(T=ys[:,3],rho=rho)
         # eps = radiative_gradient(T=ys[:,3],P=ys[:,2],l=ys[:,1],m=xs,rho=rho,optable=self.opacity)
         self.dashboard.add_density(xs,rho,integrator,append=False)
@@ -266,6 +251,10 @@ class Star(object):
         """Fraction of Heavy Elements"""
         return 1.0 - (self.X + self.Y + self.dXc + self.dXo)
     
+    @property
+    def XCNO(self):
+        """Fraction of Carbon, Oxygen and Nitrogen"""
+        return self.config["Star.Composition.ZdXCNO"] * self.Z + self.dXc + self.dXo
     
     @property
     def mu(self):
@@ -283,4 +272,8 @@ class Star(object):
     def config(self):
         """The configuration for this star."""
         return self._config
+        
+    def stop(self):
+        """End this star!"""
+        pass
         
